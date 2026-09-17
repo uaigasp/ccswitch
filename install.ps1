ea86a21d528746f94f129e695d01dc68aaf75cdd
uaigasp <gasparbasualdodiaz@gmail.com>
2026-09-17 00:08:10 -0300

    arreglar path vacio al armar la variable PATH

--- install.ps1
+++ install.ps1
@@ -18,11 +18,9 @@ Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $exePath
 
 Write-Host "agregando $installDir al PATH del usuario..."
 $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
-if (-not $currentPath) {
-    $currentPath = ""
-}
 if ($currentPath -notlike "*$installDir*") {
-    [Environment]::SetEnvironmentVariable("Path", "$currentPath;$installDir", "User")
+    $newPath = if ($currentPath) { "$currentPath;$installDir" } else { $installDir }
+    [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
 }
 
 Write-Host ""
